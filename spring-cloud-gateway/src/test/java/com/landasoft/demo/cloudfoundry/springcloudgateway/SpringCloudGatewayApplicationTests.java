924c0fde25eba8c60b31f6923efe65b74689255e
wulinyun <lin.wu@landasoft.com>
2019-11-20 18:33:57 +0800

    提交cloudfoundry的spring-cloud-gateway

---
+++ spring-cloud-gateway/src/test/java/com/landasoft/demo/cloudfoundry/springcloudgateway/SpringCloudGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.landasoft.demo.cloudfoundry.springcloudgateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloudGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
