0b94a4b63b579c60f5aa76255388712b59cd800a
wulinyun <lin.wu@landasoft.com>
2019-11-21 17:39:27 +0800

    feignclient服务类

---
+++ spring-cloud-client/src/main/java/com/landasoft/demo/cloudfoundry/springcloudclient/feign/SpringCloudClientBackendFeign.java
@@ -0,0 +1,16 @@
+package com.landasoft.demo.cloudfoundry.springcloudclient.feign;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * feign调用客户端定义  name的值为被调用服务中配置的spring.application.name的值
+ * @author 伍林云
+ * @date 2019/8/9  14:18
+ */
+@FeignClient(name = "spring-cloud-client-backend")
+public interface SpringCloudClientBackendFeign {
+    @RequestMapping("/call/{id}")
+    String call(@PathVariable("id") String id);
+}
