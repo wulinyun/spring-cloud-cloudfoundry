46971cb795b0eb82d7112e76b8adbe02fd68d133
wulinyun <lin.wu@landasoft.com>
2019-11-21 17:06:23 +0800

    加入exclude属性SecurityAutoConfiguration和ManagementWebSecurityAutoConfiguration，以排除安全认证

--- spring-cloud-client-backend/src/main/java/com/landasoft/demo/cloudfoundry/springcloudclientbackend/SpringCloudClientBackendApplication.java
+++ spring-cloud-client-backend/src/main/java/com/landasoft/demo/cloudfoundry/springcloudclientbackend/SpringCloudClientBackendApplication.java
@@ -1,10 +1,12 @@
 package com.landasoft.demo.cloudfoundry.springcloudclientbackend;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.autoconfigure.security.servlet.ManagementWebSecurityAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-
-@SpringBootApplication
+/** 警用spring security **/
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class,ManagementWebSecurityAutoConfiguration.class })
 @EnableEurekaClient
 public class SpringCloudClientBackendApplication {
 
