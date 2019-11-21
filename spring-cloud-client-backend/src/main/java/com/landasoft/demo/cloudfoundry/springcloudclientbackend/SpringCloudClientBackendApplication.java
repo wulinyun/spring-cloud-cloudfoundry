d0f47897752c27b94d3359352094570bc894eae2
wulinyun <lin.wu@landasoft.com>
2019-11-21 13:57:11 +0800

    spring-cloud-client-backend测试初始化代码

---
+++ spring-cloud-client-backend/src/main/java/com/landasoft/demo/cloudfoundry/springcloudclientbackend/SpringCloudClientBackendApplication.java
@@ -0,0 +1,13 @@
+package com.landasoft.demo.cloudfoundry.springcloudclientbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudClientBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCloudClientBackendApplication.class, args);
+    }
+
+}
