380d1c395b5c2f89ff4eece318a9883cc24f81fd
wulinyun <lin.wu@landasoft.com>
2019-11-21 17:05:07 +0800

    加入exclude以排除安全认证

--- spring-cloud-gateway/src/main/java/com/landasoft/demo/cloudfoundry/springcloudgateway/SpringCloudGatewayApplication.java
+++ spring-cloud-gateway/src/main/java/com/landasoft/demo/cloudfoundry/springcloudgateway/SpringCloudGatewayApplication.java
@@ -1,12 +1,15 @@
 package com.landasoft.demo.cloudfoundry.springcloudgateway;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.autoconfigure.security.servlet.ManagementWebSecurityAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class,
+        ManagementWebSecurityAutoConfiguration.class})
 @EnableZuulProxy
 @EnableDiscoveryClient
 public class SpringCloudGatewayApplication {
