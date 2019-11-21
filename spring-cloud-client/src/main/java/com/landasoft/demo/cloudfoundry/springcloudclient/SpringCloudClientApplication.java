2d3eb812fa3fd9deaa0ae3e53be7887cdf1b0274
wulinyun <lin.wu@landasoft.com>
2019-11-21 17:41:00 +0800

    配置两种restTemplate，一个用于使用注册中心eureka的服务名称调用其他服务，一个用于原始的http或者https调用

--- spring-cloud-client/src/main/java/com/landasoft/demo/cloudfoundry/springcloudclient/SpringCloudClientApplication.java
+++ spring-cloud-client/src/main/java/com/landasoft/demo/cloudfoundry/springcloudclient/SpringCloudClientApplication.java
@@ -4,10 +4,31 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.boot.actuate.autoconfigure.security.servlet.ManagementWebSecurityAutoConfiguration;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
 /** SpringBootApplication使用属性exclude禁用spring security **/
 @SpringBootApplication(exclude = {SecurityAutoConfiguration.class,ManagementWebSecurityAutoConfiguration.class })
 public class SpringCloudClientApplication {
+    /**
+     * 走注册中心通过服务名称调用的restTemplate
+     * @return
+     */
+    @Bean(name = {"loadBalancedRestTemplate"})
+    @LoadBalanced
+    public RestTemplate getRestTemplate(){
+        return new RestTemplate();
+    }
 
+    /**
+     * 普通的restTemplate
+     * @return
+     */
+    @Bean(name = {"originRestTemplate"})
+    public RestTemplate RestTemplate(){
+        return new RestTemplate();
+    }
     public static void main(String[] args) {
         SpringApplication.run(SpringCloudClientApplication.class, args);
     }
