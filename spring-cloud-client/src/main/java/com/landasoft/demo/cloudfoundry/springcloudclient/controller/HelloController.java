17d6c6d7c5375eb1307a6b30b5f4d13710043d71
wulinyun <lin.wu@landasoft.com>
2019-11-21 17:39:02 +0800

    提交直接调用接口，通过注册中心eureka服务名调用以及通过feignclient方式调用的测试方法

---
+++ spring-cloud-client/src/main/java/com/landasoft/demo/cloudfoundry/springcloudclient/controller/HelloController.java
@@ -0,0 +1,74 @@
+package com.landasoft.demo.cloudfoundry.springcloudclient.controller;
+import com.landasoft.demo.cloudfoundry.springcloudclient.feign.SpringCloudClientBackendFeign;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+/**
+ * @author 伍林云
+ * @date 2019/8/5  18:45
+ */
+@RestController
+@Api(description = "客户端用户管理接口")
+public class HelloController {
+
+    private static final Logger logger = LoggerFactory.getLogger(HelloController.class);
+
+
+    @Autowired
+    @Qualifier("loadBalancedRestTemplate")
+    private RestTemplate loadBalancedRestTemplate;
+
+    @Autowired
+    @Qualifier("originRestTemplate")
+    private RestTemplate originRestTemplate;
+
+    @Autowired
+    private SpringCloudClientBackendFeign springCloudClientBackendFeign;
+    /**
+     * 直接调用接口地址
+     * @param id
+     * @return
+     */
+    @ApiOperation(value = "直接调用spring-cloud-client-backend接口地址")
+    @GetMapping("/call/{id}")
+    public String callHome(@PathVariable String id){
+        logger.info("calling from spring-cloud-client-backend");
+        String result= this.originRestTemplate.getForObject("https://spring-cloud-client-backend.lorealoperationsapac.com/" + id, String.class);
+        return result+" world";
+    }
+
+    /**
+     * 通过feign调用接口地址
+     * @param id
+     * @return
+     */
+    @ApiOperation(value = "通过feign调用接口地址")
+    @GetMapping("/call_f/{id}")
+    public String callHomeF(@PathVariable String id){
+        logger.info("calling from spring-cloud-client-backend by feign");
+        String result = springCloudClientBackendFeign.call(id);
+        return result+" world by feign ";
+    }
+
+    /**
+     * 通过注册中心的服务名称调用接口
+     * @param id
+     * @return
+     */
+    @ApiOperation(value = "通过注册中心的服务名称调用接口")
+    @GetMapping("/call_e/{id}")
+    public String callHomeE(@PathVariable String id){
+        logger.info("calling from spring-cloud-client-backend by eureka");
+        //spring-cloud-client-backend的值为被调用服务中配置的spring.application.name的值
+        String result= this.loadBalancedRestTemplate.getForObject("http://spring-cloud-client-backend/call/" + id, String.class);
+        return result+" world by eureka";
+    }
+}
