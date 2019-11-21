36a9fbd5ba0b4db21cb0eecb546d118c209d918c
wulinyun <lin.wu@landasoft.com>
2019-11-21 17:17:40 +0800

    spring-cloud-client演示微服务之间通过eureka进行调用测试以及使用restTemplate直接全地址调用测试

---
+++ spring-cloud-client/src/main/java/com/landasoft/demo/cloudfoundry/springcloudclient/SpringCloudClientApplication.java
@@ -0,0 +1,15 @@
+package com.landasoft.demo.cloudfoundry.springcloudclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.boot.actuate.autoconfigure.security.servlet.ManagementWebSecurityAutoConfiguration;
+/** 警用spring security **/
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class,ManagementWebSecurityAutoConfiguration.class })
+public class SpringCloudClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCloudClientApplication.class, args);
+    }
+
+}
