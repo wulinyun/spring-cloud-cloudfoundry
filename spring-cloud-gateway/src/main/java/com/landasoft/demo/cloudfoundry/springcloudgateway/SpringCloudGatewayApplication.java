b37190b4429b11f108731309fb49d9773953634b
wulinyun <lin.wu@landasoft.com>
2019-11-21 13:44:26 +0800

    更新发觉注册中心注解

--- spring-cloud-gateway/src/main/java/com/landasoft/demo/cloudfoundry/springcloudgateway/SpringCloudGatewayApplication.java
+++ spring-cloud-gateway/src/main/java/com/landasoft/demo/cloudfoundry/springcloudgateway/SpringCloudGatewayApplication.java
@@ -2,8 +2,13 @@ package com.landasoft.demo.cloudfoundry.springcloudgateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 @SpringBootApplication
+@EnableZuulProxy
+@EnableDiscoveryClient
 public class SpringCloudGatewayApplication {
 
     public static void main(String[] args) {
